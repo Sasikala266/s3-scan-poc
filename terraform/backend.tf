3533982498f2fa1b1badd9cb1635dd54f4f65471
Sasikala266 <tippanagarimanasa@gmail.com>
2026-05-08 16:12:21 +0530

    reving the DynamoDB table

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -7,6 +7,5 @@ terraform {
     key            = "sasi-s3-scan-poc/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-state-lock"
   }
 }
