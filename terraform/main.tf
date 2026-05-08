aa36e8bf0eb85d671508fb03bb598a5f04bad93d
Sasikala266 <tippanagarimanasa@gmail.com>
2026-05-08 16:26:32 +0530

    clean up

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,14 +36,6 @@ resource "aws_s3_bucket_website_configuration" "ui" {
   }
 }
 
-#resource "aws_s3_bucket_public_access_block" "ui" {
-  #bucket = aws_s3_bucket.ui.id
-
-  #block_public_acls       = false
-  #block_public_policy     = false
-  #ignore_public_acls      = false
-  #restrict_public_buckets = false
-#}
 
 data "aws_iam_policy_document" "ui_public_read" {
   statement {
@@ -64,7 +56,6 @@ resource "aws_s3_bucket_policy" "ui" {
   bucket = aws_s3_bucket.ui.id
   policy = data.aws_iam_policy_document.ui_public_read.json
 
-  #depends_on = [aws_s3_bucket_public_access_block.ui]
 }
 
 # Upload UI index.html
